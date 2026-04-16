041127cb0c375f6bd84a27fac0ef962ad7b63b53
paoladisouza4 <paoladisouza4@gmail.com>
2026-04-16 17:25:24 -0300

    Add files via upload

---
+++ src/documents/aluguel_res/meta.js
@@ -0,0 +1,11 @@
+export const meta = {
+  id: 'aluguel_res',
+  emoji: '🏠',
+  title: 'Contrato de Locação Residencial',
+  category: 'imobiliario',
+  roleA: 'LOCADOR',
+  roleB: 'LOCATÁRIO',
+  parteB: true,
+  testemunhas: true,
+  steps: [1,2,3,4,5,6],
+}
