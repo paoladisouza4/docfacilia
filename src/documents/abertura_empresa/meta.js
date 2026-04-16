ccd72a81ee722ddcf7a7f623c525670fa23c94cf
paoladisouza4 <paoladisouza4@gmail.com>
2026-04-16 00:09:22 -0300

    Add files via upload

---
+++ src/documents/abertura_empresa/meta.js
@@ -0,0 +1,11 @@
+export const meta = {
+  id: 'abertura_empresa',
+  emoji: '🏢',
+  title: 'Requerimento de Abertura de Empresa',
+  category: 'empresarial',
+  roleA: 'RESPONSÁVEL',
+  roleB: '',
+  parteB: false,
+  testemunhas: false,
+  steps: [1,2,3,6],
+}
