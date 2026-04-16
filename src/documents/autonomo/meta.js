a3bb6807aba24d03a40fcc376cf865b865ef10c1
paoladisouza4 <paoladisouza4@gmail.com>
2026-04-16 17:27:22 -0300

    Add files via upload

---
+++ src/documents/autonomo/meta.js
@@ -0,0 +1,11 @@
+export const meta = {
+  id: 'autonomo',
+  emoji: '🔧',
+  title: 'Acordo de Prestação de Serviços Autônomos',
+  category: 'contratos',
+  roleA: 'CONTRATANTE',
+  roleB: 'PRESTADOR',
+  parteB: true,
+  testemunhas: false,
+  steps: [1,2,3,4,5,6],
+}
